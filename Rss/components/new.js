c8d481068b7b959527073959f00c2518f933d12d
jjcr11 <jjcr0397@gmail.com>
2021-02-25 19:54:51 -0600

    Add buttons to navigate between news

--- Rss/components/new.js
+++ Rss/components/new.js
@@ -1,4 +1,4 @@
-/*import React from 'react';
+import React from 'react';
 import {
   ScrollView,
   Text,
@@ -10,134 +10,76 @@ import {
 import {Dimensions} from 'react-native';
 import HTMLView from 'react-native-htmlview';
 import {SwiperFlatList} from 'react-native-swiper-flatlist';
+import {WebView} from 'react-native-webview';
 
 const {width, height} = Dimensions.get('window');
-
-const DATA = [];
-
-const getItem = (data, index) => ({
-  id: Math.random().toString(12).substring(0),
-  title: `Item ${index + 1}`,
-});
-
-const getItemCount = (data) => 50;
 
 class ItemNewList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      //content: props.content,
-      //index: props.index,
-      title: props.title,
+      content: props.content,
+      index2: props.index2,
     };
   }
   render() {
-    return (
-      <ScrollView style={{width: width, minHeight: height * 1.5}}>
-        <Text>{title}</Text>
-      </ScrollView>
-    );
+    return <WebView source={{html: this.state.content}} />;
   }
 }
 
 class New extends React.Component {
   constructor(props) {
     super(props);
+    this.nextPage = this.nextPage.bind(this);
+    this.previousPage = this.previousPage.bind(this);
     this.state = {
       allFeed: props.route.params.allFeed,
       index: props.route.params.index,
     };
   }
+  previousPage() {
+    this.setState({index: this.state.index - 1});
+    console.log(this.state.index);
+  }
+  nextPage() {
+    this.setState({index: this.state.index + 1});
+    console.log(this.state.index);
+  }
   render() {
     return (
       <View>
-        <VirtualizedList
-          data={DATA}
-          initialNumToRender={4}
-          renderItem={({item}) => <ItemNewList title={item.title} />}
-          keyExtractor={(item) => item.key}
-          getItemCount={getItemCount}
-          getItem={getItem}
+        <FlatList
+          snapToInterval={0}
+          disableIntervalMomentum={true}
+          scroll
+          decelerationRate={'fast'}
+          scrollEnabled={false}
+          horizontal={true}
+          contentOffset={{x: width * this.state.index, y: 0}}
+          initialNumToRender={this.state.allFeed.length}
+          data={this.state.allFeed}
+          renderItem={(item) => (
+            <View style={{width: width, height: height * 0.7}}>
+              <ItemNewList content={item.item.description} />
+              <View
+                style={{
+                  backgroundColor: 'red',
+                  justifyContent: 'space-between',
+                  flexDirection: 'row',
+                }}>
+                <TouchableOpacity onPress={this.previousPage}>
+                  <Text>Previus page</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.nextPage}>
+                  <Text>Next page</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
         />
       </View>
     );
   }
 }
-
-export default New;
-*/
-
-import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  VirtualizedList,
-  StyleSheet,
-  Text,
-  StatusBar,
-  ScrollView,
-} from 'react-native';
-import HTMLView from 'react-native-htmlview';
-import {Dimensions} from 'react-native';
-
-const {width, height} = Dimensions.get('window');
-
-const DATA = [];
-
-const getItem = (data, index) => ({
-  id: Math.random().toString(12).substring(0),
-  title: `Item ${index + 1}`,
-});
-
-const getItemCount = (data) => 50;
-
-const Item = ({title}) => {
-  //console.log(title);
-  return (
-    <ScrollView style={{width: width, minHeight: height}}>
-      <HTMLView value={title} />
-    </ScrollView>
-  );
-};
-
-const New = ({route}) => {
-  //console.log(route.params.allFeed.length);
-  return (
-    <View>
-      <VirtualizedList
-        snapToInterval={width}
-        decelerationRate="fast"
-        horizontal={true}
-        contentOffset={{x: width * route.params.index, y: 0}}
-        initialNumToRender={route.params.allFeed.length}
-        data={route.params.allFeed}
-        windowSize={5}
-        //initialNumToRender={4}
-        renderItem={({item}) => <Item title={item.title} />}
-        getItemCount={(data) => route.params.allFeed.length}
-        getItem={(data, index) => ({
-          id: Math.random().toString(12).substring(0),
-          title: data[index].content || data[index].description,
-        })}
-      />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    //flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    //justifyContent: 'center',
-    width: width,
-    minHeight: height * 1.5,
-  },
-  title: {
-    fontSize: 32,
-  },
-});
 
 export default New;
